00f28a834a64c41a3552be195afcb63dc13feda0
ondrejhonus <honusondrej@gmail.com>
2024-09-22 13:12:09 +0200

    it works, I aint doin more

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+#define ledPin 2
+#define sensorPin A0
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+}
+
+void loop() { 
+  int output_val = map(analogRead(sensorPin), 0, 1023, 255, 0);
+  Serial.println(output_val);
+  if (output_val < 40){
+    digitalWrite(ledPin, 1);
+  }
+  else{
+        digitalWrite(ledPin, 0);
+  }
+  delay(500);
+}
